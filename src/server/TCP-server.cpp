2c773d3788ce3be457d2e03cf2cc5b63f22687b9
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-07-31 23:34:05 +0100

    InitializeSocket() InitializeAddress() and ConvertAddrBinary()

--- src/server/TCP-server.cpp
+++ src/server/TCP-server.cpp
@@ -17,3 +17,39 @@
 #include <iostream>
 
 #include "TCP-server.hpp"
+
+/// @brief Constructor of the class TCPServer
+/// @param port 
+/// @param address 
+TCPServer::TCPServer(const unsigned short port, const std::string& address) {
+
+}
+
+/// @brief Initialize the file descriptor for the socket
+void TCPServer::InitializeSocket() {
+  socket_fd_ = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_fd_ < 0) {
+    throw(InitializeSocketException());
+  }
+}
+
+/// @brief Initialize the address of the server
+/// @param port 
+/// @param addr 
+void TCPServer::InitializeAddress(const unsigned short port, const in_addr& addr) {
+  memset(&addr_, 0, sizeof(addr_));
+  addr_.sin_family = AF_INET;
+  addr_.sin_addr = addr;
+  addr_.sin_port = htons(port);
+}
+
+/// @brief Converts a IPv4 number and dots notation into binary
+/// @param address 
+/// @return 
+in_addr TCPServer::ConvertAddrBinary(const std::string& address) {
+  in_addr addr;
+  if (inet_aton(address.c_str(), &addr) < 0) {
+    throw(ConvertBinaryAddrException());
+  }
+  return addr;
+}
--- src/server/TCP-server.hpp
+++ src/server/TCP-server.hpp
@@ -10,9 +10,63 @@
 #ifndef TCP_SERVER
 #define TCP_SERVER
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include <exception>
 #include <string>
 
+/// @brief Abstraction of a TCP server.
+class TCPServer {
+  public:
+    // Special methods
+    TCPServer() = delete;
+    TCPServer(const TCPServer&) = delete;
+    TCPServer& operator=(const TCPServer&) = delete;
+    TCPServer(TCPServer&&) = delete;
+    TCPServer& operator=(TCPServer&&) = delete;
 
+    TCPServer(const unsigned short port, const std::string& addr);
+    ~TCPServer();
+    
+    // Normal methods
+
+  private:
+    // Private methods
+    void InitializeSocket();
+    void InitializeAddress(const unsigned short port, const in_addr& addr) noexcept;
+    static in_addr ConvertAddrBinary(const std::string& addr);
+
+
+
+    // Attributes
+    sockaddr_in addr_;
+    int socket_fd_;
+    unsigned char* buffer_;
+    size_t buffer_size_;
+};
+
+/// @brief Exceptions of TCPServer
+class TCPServerException : public std::exception {
+  public:
+    TCPServerException(const std::string& message) : message_(message) {}
+    const char* what() const noexcept override {return message_.c_str();}
+  private:
+    std::string message_;
+};
+
+
+/// @brief represents an error intializing the socket file descriptor.
+class InitializeSocketException : public TCPServerException {
+  public:
+    InitializeSocketException() : TCPServerException("Error initializing the socket file descriptor") {}
+};
+
+/// @brief represents an error converting from string to binary a IPv4 address.
+class ConvertBinaryAddrException : public TCPServerException {
+  public:
+    ConvertBinaryAddrException() : TCPServerException("Error converting the string address to binary") {}
+};
 
 #endif
