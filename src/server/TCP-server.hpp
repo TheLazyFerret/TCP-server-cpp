f53836e3ae1b8281aa4765820844fad386f6e2e8
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-08-03 09:53:20 +0100

    first commit

--- src/server/TCP-server.hpp
+++ src/server/TCP-server.hpp
@@ -10,101 +10,31 @@
 #ifndef TCP_SERVER_HPP
 #define TCP_SERVER_HPP
 
-#include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 
 #include <exception>
 #include <string>
 #include <cstring>
 
-/// @brief Abstraction of a TCP server.
 class TCPServer {
   public:
-    // Special methods
+    // SPECIAL METHODS
     TCPServer() = delete;
     TCPServer(const TCPServer&) = delete;
     TCPServer& operator=(const TCPServer&) = delete;
     TCPServer(TCPServer&&) = delete;
     TCPServer& operator=(TCPServer&&) = delete;
 
-    TCPServer(const unsigned short port, const std::string& addr, const size_t buffer_size = 0);
+    TCPServer(const unsigned short port, const std::string& address);
     ~TCPServer();
-    
-    // Normal methods
-    void Initialize();
-    sockaddr_in Accept();
-
-    size_t SetBuffer(const void* ext, const size_t n_bytes = 0) noexcept;
-    size_t Send(const size_t n_bytes = 0, const int flags = 0);
-
-    size_t Recv(const size_t n_bytes = 0, const int flags = 0);
-    size_t GetBuffer(void* dest, const size_t n_bytes = 0) const noexcept;
-
-    void Kill() noexcept;
+ 
+    // NORMAL METHODS
 
   private:
-    // Private methods
-    static in_addr ConvertAddrBinary(const std::string& addr);
+    // PRIVATE METHODS
 
-    // Attributes
-    sockaddr_in addr_;
-    int socket_fd_;
-    unsigned char* send_buffer_;
-    unsigned char* recv_buffer_;
-    size_t buffer_size_;
-};
+    // ATTRIBUTES
 
-
-/// @brief Exceptions of TCPServer
-class TCPServerException : public std::exception {
-  public:
-    TCPServerException(const std::string& message) : message_(message) {}
-    const char* what() const noexcept override {return message_.c_str();}
-  private:
-    std::string message_;
-};
-
-/// @brief Represents an error intializing the socket file descriptor.
-class InitializeSocketException : public TCPServerException {
-  public:
-    InitializeSocketException(const int error_code) : TCPServerException(std::string(strerror(error_code))) {}
-};
-
-/// @brief Represents an error converting from string to binary a IPv4 address.
-class ConvertBinaryAddrException : public TCPServerException {
-  public:
-    ConvertBinaryAddrException(const int error_code) : TCPServerException(std::string(strerror(error_code))) {}
-};
-
-/// @brief Represents an error binding the socket.
-class BindingException : public TCPServerException {
-  public:
-    BindingException(const int error_code) : TCPServerException(std::string(strerror(error_code))) {}
-};
-
-/// @brief Represents an error setting the socket in passive mode.
-class ListeningException : public TCPServerException {
-  public:
-    ListeningException(const int error_code) : TCPServerException(std::string(strerror(error_code))) {}
-};
-
-/// @brief Represents an error accepting a connection.
-class AcceptException : public TCPServerException {
-  public:
-    AcceptException(const int error_code) : TCPServerException(std::string(strerror(error_code))) {}
-};
-
-/// @brief Represents an error sending the message.
-class SendException : public TCPServerException {
-  public:
-    SendException(const int error_code) : TCPServerException(std::string(strerror(error_code))) {}
-};
-
-/// @brief Represents an error recovering the message.
-class RecvException : public TCPServerException {
-  public:
-    RecvException(const int error_code) : TCPServerException(std::string(strerror(error_code))) {}
 };
 
 #endif
