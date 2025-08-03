05c8f90f5dc2edd70202e1db6810df07274a7050
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-08-03 14:53:33 +0100

    Move socket file descriptor creation to initialize. Initialize()

--- src/server/TCP-server.cpp
+++ src/server/TCP-server.cpp
@@ -26,28 +26,52 @@
 
 constexpr int KDefault_Backlog = 5;
 
-/// @brief Constructor of TCPServer
+/// @brief Constructor of TCPServer.
 /// @param port 
 /// @param address 
 TCPServer::TCPServer(const unsigned short port, const std::string& address) {
+  // Initialize attributes.
   initialized_ = false;
+  socket_fd_ = -1;
+  memset(&socket_addr_, 0, sizeof(socket_addr_));
+
+  // Initialize address.
+  socket_addr_.sin_family = AF_INET;
+  socket_addr_.sin_port = htons(port);
+  socket_addr_.sin_addr = ConvertAddrBinary(address);
+}
+
+/// @brief Initialize the server.
+///   Create the socket file descriptor, bind to the address and set to passive mode.
+/// @param backlog 
+void TCPServer::Initialize(int backlog) {
   // Initialize socket file descriptor.
   socket_fd_ = socket(AF_INET, SOCK_STREAM, 0);
   if (socket_fd_ < 0) {
     throw ErrnoException(errno);
   }
   DEBUG_PRINT("Socket file descriptor created with code: " << socket_fd_);
-  
-  // Initialize address.
-  memset(&socket_addr_, 0, sizeof(socket_addr_));
-  socket_addr_.sin_family = AF_INET;
-  socket_addr_.sin_port = htons(port);
-  socket_addr_.sin_addr = ConvertAddrBinary(address);
+
+  // Bind the socket file descriptor to an address.
+  const sockaddr* aux_addr_pointer = reinterpret_cast<sockaddr*>(&socket_addr_);
+  const socklen_t aux_addr_len = static_cast<socklen_t>(sizeof(socket_addr_));
+  if (bind(socket_fd_, aux_addr_pointer, aux_addr_len) < 0) {
+    throw ErrnoException(errno);
+  }
+  DEBUG_PRINT("Socket bound to the port: " << ntohs(socket_addr_.sin_port));
+
+  // Set the socket to passive mode.
+  if (backlog <= 0) backlog = KDefault_Backlog;
+  if (listen(socket_fd_, backlog)) {
+    throw ErrnoException(errno);
+  }
+  DEBUG_PRINT("Socket set to passive mode");
+  initialized_ = true;
 }
 
-/// @brief Convert a string address into a binary (in network order) address
+/// @brief Convert a string address into a binary (in network order) address.
 /// @param address 
-/// @return the address in binary
+/// @return The address in binary.
 in_addr TCPServer::ConvertAddrBinary(const std::string& address) {
   in_addr addr;
   if (inet_aton(address.c_str(), &addr) < 0) {
@@ -55,15 +79,4 @@ in_addr TCPServer::ConvertAddrBinary(const std::string& address) {
   }
   DEBUG_PRINT("Address converted from: " << address << " to: " << ntohl(addr.s_addr));
   return addr;
-}
-
-void TCPServer::Initialize(const int backlog) {
-  // Bind the socket file descriptor to an address.
-  const sockaddr* aux_addr_pointer = reinterpret_cast<sockaddr*>(&socket_addr_);
-  const socklen_t aux_addr_len = static_cast<socklen_t>(sizeof(socket_addr_));
-  if (bind(socket_fd_, aux_addr_pointer, aux_addr_len) < 0) {
-    throw ErrnoException(errno);
-  }
-  // Set the socket to passive mode.
-  
 }
